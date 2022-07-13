06c30334faa946c7369974c62c79c3b56b06ee4a
williambl <william@williambl.com>
2022-07-13 02:51:05 +0100

    feat: implement message sending on forge

--- Forge/src/main/java/com/williambl/portablejukebox/PortableJukeboxMod.java
+++ Forge/src/main/java/com/williambl/portablejukebox/PortableJukeboxMod.java
@@ -3,15 +3,20 @@ package com.williambl.portablejukebox;
 import com.williambl.portablejukebox.jukebox.PortableJukeboxItem;
 import com.williambl.portablejukebox.jukebox.PortableJukeboxLoadRecipe;
 import com.williambl.portablejukebox.noteblock.PortableNoteBlockItem;
+import com.williambl.portablejukebox.platform.ForgeMessageSender;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SimpleRecipeSerializer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import static com.williambl.portablejukebox.PortableJukeboxCommon.id;
 
 @Mod(Constants.MOD_ID)
 public class PortableJukeboxMod {
@@ -27,5 +32,7 @@ public class PortableJukeboxMod {
 
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         RECIPE_SERIALIZERS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        ForgeMessageSender.init();
     }
 }
