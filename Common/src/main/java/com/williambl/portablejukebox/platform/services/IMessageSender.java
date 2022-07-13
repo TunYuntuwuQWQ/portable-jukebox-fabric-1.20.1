08c3fb3145d3b1907efd424cf00703fbba9bbe91
williambl <william@williambl.com>
2022-07-13 02:50:45 +0100

    refactor: change sendMessage's second arg

--- Common/src/main/java/com/williambl/portablejukebox/platform/services/IMessageSender.java
+++ Common/src/main/java/com/williambl/portablejukebox/platform/services/IMessageSender.java
@@ -2,7 +2,8 @@ package com.williambl.portablejukebox.platform.services;
 
 import com.williambl.portablejukebox.jukebox.PortableJukeboxMessage;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 
 public interface IMessageSender {
-    void sendMessage(PortableJukeboxMessage message, ServerPlayer... players);
+    void sendMessage(PortableJukeboxMessage message, Entity tracking);
 }
--- Fabric/src/main/java/com/williambl/portablejukebox/platform/FabricMessageSender.java
+++ Fabric/src/main/java/com/williambl/portablejukebox/platform/FabricMessageSender.java
@@ -3,16 +3,18 @@ package com.williambl.portablejukebox.platform;
 import com.williambl.portablejukebox.jukebox.PortableJukeboxMessage;
 import com.williambl.portablejukebox.platform.services.IMessageSender;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 
 public class FabricMessageSender implements IMessageSender {
     @Override
-    public void sendMessage(PortableJukeboxMessage message, ServerPlayer... players) {
+    public void sendMessage(PortableJukeboxMessage message, Entity tracking) {
         var buf = PacketByteBufs.create();
         message.encode(buf);
 
-        for (var player : players) {
+        for (var player : PlayerLookup.tracking(tracking)) {
             ServerPlayNetworking.send(player, PortableJukeboxMessage.MESSAGE_ID, buf);
         }
     }
